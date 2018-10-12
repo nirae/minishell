2161459be484eca90a57d3c1061c2d91989fb216
ndubouil <ndubouil@student.42.fr>
2018-10-12 21:19:36 +0200

    unsetenv OK

--- src/main.c
+++ src/main.c
@@ -6,59 +6,59 @@
 /*   By: ndubouil <ndubouil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/27 01:02:06 by ndubouil          #+#    #+#             */
-/*   Updated: 2018/10/11 19:27:37 by ndubouil         ###   ########.fr       */
+/*   Updated: 2018/10/12 21:14:15 by ndubouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// void	catch_signal_ctrl_c(int signal)
-// {
-// 	if (signal == SIGINT)
-// 		ft_printf("\n%s", PROMPT);
-// }
-//
-// void	error()
-// {
-// 	ft_printf("minishell failed\n");
-// 	exit(EXIT_FAILURE);
-// }
+void	catch_signal(int signal)
+{
+	if (signal == SIGINT)
+		ft_printf("\n%s", PROMPT);
+}
+
+void	error()
+{
+	ft_printf("minishell failed\n");
+	exit(EXIT_FAILURE);
+}
 
 /*
 **	Return the complete path of a file name or exit if malloc failed
 */
 
-// static char	*get_complete_path(char *parent, char *name)
-// {
-// 	char	*tmp;
-// 	char	*result;
-//
-// 	if (name[0] == '/' || name[0] == '.')
-// 		return (name);
-// 	if (parent[ft_strlen(parent) - 1] == '/')
-// 	{
-// 		if (!(tmp = ft_strdup(parent)))
-// 			error();
-// 	}
-// 	else
-// 	{
-// 		if (!(tmp = ft_strjoin(parent, "/")))
-// 			error();
-// 	}
-// 	if (!(result = ft_strjoin(tmp, name)))
-// 		error();
-// 	ft_strdel(&tmp);
-// 	return (result);
-// }
-//
-// int		minishell_parser(char *input, char ***command)
-// {
-// 	if (input[0] == 0)
-// 		return (0);
-// 	if (!(*command = ft_strsplit(input, ' ')))
-// 		return (0);
-// 	return (1);
-// }
+static char	*get_complete_path(char *parent, char *name)
+{
+	char	*tmp;
+	char	*result;
+
+	if (name[0] == '/' || name[0] == '.')
+		return (name);
+	if (parent[ft_strlen(parent) - 1] == '/')
+	{
+		if (!(tmp = ft_strdup(parent)))
+			error();
+	}
+	else
+	{
+		if (!(tmp = ft_strjoin(parent, "/")))
+			error();
+	}
+	if (!(result = ft_strjoin(tmp, name)))
+		error();
+	ft_strdel(&tmp);
+	return (result);
+}
+
+int		minishell_parser(char *input, char ***command)
+{
+	if (input[0] == 0)
+		return (0);
+	if (!(*command = ft_strsplit(input, ' ')))
+		return (0);
+	return (1);
+}
 
 /*
 **	A proteger!!!!!!
@@ -79,21 +79,17 @@
 	return (my_env);
 }*/
 
-t_varenv	*create_varenv(char *envline)
+t_varenv	*create_varenv(char *name, char *content)
 {
 	t_varenv	*varenv;
-	char		**tmp;
 
 	// Gerer l'erreur
-	if (!(varenv = ft_memalloc(sizeof(t_varenv *))))
+	if (!(varenv = ft_memalloc(sizeof(t_varenv *) + (sizeof(char *) * 2))))
 		exit(0);
-	// A gerer
-	if (!(tmp = ft_strsplit(envline, '=')))
+	if (!(varenv->name = ft_strdup(name)))
 		exit(0);
-	varenv->name = tmp[0];
-	ft_printf("name here = %s\n", varenv->name);
-	varenv->content = tmp[1];
-	ft_memdel((void **)&tmp);
+	if (!(varenv->content = ft_strdup(content)))
+		exit(0);
 	return (varenv);
 }
 
@@ -101,195 +97,246 @@ void		create_my_env(t_list **lst, char **environ)
 {
 	int		i;
 	t_list	*tmplst;
+	char	**env;
 
 	i = 0;
 	while (environ[i])
 	{
 		// a proteger
-		tmplst = ft_lstnew(NULL, sizeof(t_varenv *));
-		tmplst->content = create_varenv(environ[i]);
+		if (!(tmplst = ft_lstnew(NULL, sizeof(t_varenv *))))
+			ft_printf("lstnew a foire\n");
+		if (!(env = ft_strsplit(environ[i], '=')))
+			ft_printf("strsplit a foire\n");
+		tmplst->content = create_varenv(env[0], env[1]);
 		if (!(*lst))
 			*lst = tmplst;
 		else
 			ft_lstadd(lst, tmplst);
+		ft_memdel((void **)&env);
 		i++;
 	}
 }
 
+char		**get_env_paths(t_list *lst)
+{
+	t_list	*tmp;
+	char	**env_paths;
+
+	tmp = lst;
+	while (lst)
+	{
+		if (ft_strcmp((((t_varenv *)(tmp->content))->name), "PATH") == 0)
+			break;
+		if (tmp->next == NULL)
+			return (NULL);
+		tmp = tmp->next;
+	}
+	env_paths = ft_strsplit(((t_varenv *)(tmp->content))->content, ':');
+	return (env_paths);
+}
+
+int			change_env_var(t_list **lst, char *name, char *newcontent)
+{
+	t_list	*tmp;
+
+	tmp = *lst;
+	while (tmp)
+	{
+		if (ft_strcmp((((t_varenv *)((tmp)->content))->name), name) == 0)
+			break;
+		if ((tmp)->next == NULL)
+			return (FALSE);
+		(tmp) = (tmp)->next;
+	}
+	((t_varenv *)((tmp)->content))->content = newcontent;
+	return (TRUE);
+}
+
+void		del_lst(void *content, size_t size)
+{
+	(void)size;
+	ft_strdel(&((t_varenv *)(content))->name);
+	ft_strdel(&((t_varenv *)(content))->content);
+}
+
+int			remove_one(t_list **lst, char *name)
+{
+	t_list	*prev;
+//	t_list	*next;
+	t_list	*tmp;
+
+
+	tmp = *lst;
+	if (tmp && ft_strcmp(((t_varenv *)(tmp->content))->name, name) == 0)
+	{
+		prev = tmp;
+		*lst = tmp->next;
+		ft_lstdelone(&prev, del_lst);
+		return (1);
+	}
+	while(tmp)
+	{
+		if (tmp->next && ft_strcmp(((t_varenv *)(tmp->next->content))->name, name) == 0)
+		{
+				prev = tmp->next;
+				tmp->next = tmp->next->next;
+				ft_lstdelone(&prev, del_lst);
+				return (1);
+		}
+//		prev = tmp->next;
+//		next = tmp->next;
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
 void		print_lstenv(t_list *lst)
 {
-	ft_printf("coucou");
-	ft_printf("name = %s, content = %s\n", ((t_varenv *)(lst->content))->name, ((t_varenv *)(lst->content))->content);
+	ft_printf("%s=%s\n", ((t_varenv *)(lst->content))->name, ((t_varenv *)(lst->content))->content);
 }
 
 int			main(int ac, char **av, char **environ)
 {
-	// pid_t	father;
-	// char	*line;
-	// char	**command;
-	// char	**env_path;
-	 t_list	*env;
-	// char	**tmp;
-	// struct stat st;
-	// int		i;
-	// int 	y;
-	// char 	str[PATH_MAX + 1];
+	pid_t	father;
+	char	*line;
+	char	**command;
+	char	**env_paths;
+	t_list	*env;
+	t_list	*tmplst;
+	struct stat st;
+	int		i;
+	int 	y;
+	char 	str[PATH_MAX + 1];
 
 	(void)ac;
 	(void)av;
 	env = NULL;
 	// copie de environ
 	create_my_env(&env, environ);
-	ft_printf(" TEST name = %s, content = %s\n", ((t_varenv *)(env->content))->name, ((t_varenv *)(env->content))->content);
-	ft_lstiter(env, print_lstenv);
+	//ft_lstiter(env, print_lstenv);
+	env_paths = get_env_paths(env);
 	/*
-	**	Recupere tous les repos de PATH dans env_path
+	** Boucle infinie du programme
 	*/
-	// i = -1;
-	// while (env[++i])
-	// {
-	// 	tmp = ft_strsplit(env[i], '=');
-	// 	if (ft_strcmp(tmp[0], "PATH") == 0)
-	// 		env_path = ft_strsplit(tmp[1], ':');
-	// 	ft_memdel((void **)&tmp);
-	// }
-	// /*
-	// ** Boucle infinie du programme
-	// */
-	// while (666)
-	// {
-	// 	/*// Affiche le prompt
-	// 	ft_printf("%s", PROMPT);*/
-	// 	// Surveille le signal de CTRL C
-	// 	signal(SIGINT, catch_signal_ctrl_c);
-	// 	// Affiche le prompt
-	// 	ft_printf("\n%s", PROMPT);
-	// 	// Lis l'entree standard
-	// 	line = NULL;
-	// 	if (get_next_line(0, &line) < 0)
-	// 		error();
-	// 	/*
-	// 	**	PARSING
-	// 	*/
-	// 	if (!(minishell_parser(line, &command)))
-	// 		continue;
-	// 	/*
-	// 	**	GESTION DES BULTINS
-	// 	*/
-	// 	if (ft_strcmp(command[0], "exit") == 0)
-	// 	{
-	// 		ft_printf("c'est ca degage\n");
-	// 		if (command[1])
-	// 			exit(ft_atoi(command[1]));
-	// 		else
-	// 			exit(EXIT_SUCCESS);
-	// 	}
-	// 	else if (ft_strcmp(command[0], "mypwd") == 0)
-	// 	{
-	// 		getcwd(str, PATH_MAX + 1);
-	// 		ft_printf("pwd : %s\n", str);
-	// 		continue;
-	// 	}
-	// 	else if (ft_strcmp(command[0], "cd") == 0)
-	// 	{
-	// 		char 	oldpwd[PATH_MAX + 1];
-	// 		// recupere le pwd
-	// 		getcwd(oldpwd, PATH_MAX + 1);
-	// 		if ((chdir(command[1])) < 0)
-	// 			ft_printf("chdir failed\n");
-	// 		else
-	// 		{
-	//
-	// 			// Mettre a jour PWD et OLDPWD dans l'env
-	// 			// OLDPWD
-	// 			i = -1;
-	// 			while (env[++i])
-	// 			{
-	// 				tmp = ft_strsplit(env[i], '=');
-	// 				if (ft_strcmp(tmp[0], "OLDPWD") == 0)
-	// 				{
-	// 					ft_memdel((void **)&env[i]);
-	// 					if (!(env[i] = ft_strjoin("OLDPWD=", oldpwd)))
-	// 						ft_printf("strjoin failed\n");
-	// 				}
-	// 			}
-	// 			// PWD
-	// 			char 	pwd[PATH_MAX + 1];
-	// 			// recupere le pwd
-	// 			getcwd(pwd, PATH_MAX + 1);
-	// 			i = -1;
-	// 			while (env[++i])
-	// 			{
-	// 				tmp = ft_strsplit(env[i], '=');
-	// 				if (ft_strcmp(tmp[0], "PWD") == 0)
-	// 				{
-	// 					ft_memdel((void **)&env[i]);
-	// 					if (!(env[i] = ft_strjoin("PWD=", pwd)))
-	// 						ft_printf("strjoin failed\n");
-	// 				}
-	// 			}
-	// 			//
-	// 			ft_printf("moving to %s\n", command[1]);
-	// 		}
-	// 		continue;
-	// 	}
-	// 	else if (ft_strcmp(command[0], "env") == 0)
-	// 	{
-	// 		int y = -1;
-	// 		if (env)
-	// 		{
-	// 			while (env[++y])
-	// 				ft_printf("%s\n", env[y]);
-	// 		}
-	// 		continue;
-	// 	}
-	// 	else if (ft_strcmp(command[0], "echo") == 0)
-	// 	{
-	// 		y = 0;
-	// 		while (command[1 + y])
-	// 		{
-	// 			ft_printf("%s", command[1 + y]);
-	// 			y++;
-	// 			if (command[1 + y])
-	// 				ft_printf(" ");
-	// 		}
-	// 		ft_printf("\n");
-	// 		continue;
-	// 	}
-	// 	/*
-	// 	**	FIN DES BUILTINS
-	// 	*/
-	// 	// Recherche du fichier dans les repos du path
-	// 	i = -1;
-	// 	while (env_path[++i])
-	// 	{
-	// 		// Si le fichier existe pas dans ce path, continue a boucler
-	// 		if ((lstat(get_complete_path(env_path[i], command[0]), &st)) < 0)
-	// 		{
-	// 			// Si c'etait le dernier -> pas trouve
-	// 			if (!env_path[i + 1])
-	// 			{
-	// 				ft_printf("minishell: command not found: %s\n", command[0]);
-	// 				break;
-	// 			}
-	// 			continue;
-	// 		}
-	// 		// Si le fichier est trouvé on l'execute
-	// 		else
-	// 		{
-	// 			ft_printf("tiens ton resultat de merde :\n");
-	// 			father = fork();
-	// 			if (father == 0)
-	// 				execve(get_complete_path(env_path[i], command[0]), command, env);
-	// 			else if (father < 0)
-	// 				ft_printf("fail\n");
-	// 			else if (father > 0)
-	// 				wait(&father);
-	// 			break;
-	// 		}
-	// 	}
-	// 	//ft_printf("tiens ton resultat de merde : %s\n", line);
-	// 	ft_strdel(&line);
-	// }
+	while (666)
+	{
+		/*// Affiche le prompt
+		ft_printf("%s", PROMPT);*/
+		// Surveille le signal de CTRL C
+		signal(SIGINT, catch_signal);
+		// Affiche le prompt
+		ft_printf("\n%s", PROMPT);
+		// Lis l'entree standard
+		line = NULL;
+		if (get_next_line(0, &line) < 0)
+			error();
+		/*
+		**	PARSING
+		*/
+		if (!(minishell_parser(line, &command)))
+			continue;
+		/*
+		**	GESTION DES BULTINS
+		*/
+		if (ft_strcmp(command[0], "exit") == 0)
+		{
+			ft_printf("c'est ca degage\n");
+			if (command[1])
+				exit(ft_atoi(command[1]));
+			else
+				exit(EXIT_SUCCESS);
+		}
+		else if (ft_strcmp(command[0], "mypwd") == 0)
+		{
+			getcwd(str, PATH_MAX + 1);
+			ft_printf("pwd : %s\n", str);
+			continue;
+		}
+		else if (ft_strcmp(command[0], "cd") == 0)
+		{
+			char 	oldpwd[PATH_MAX + 1];
+			// recupere le pwd
+			getcwd(oldpwd, PATH_MAX + 1);
+			if ((chdir(command[1])) < 0)
+				ft_printf("chdir failed\n");
+			else
+			{
+				if (!(change_env_var(&env, "OLDPWD", oldpwd)))
+					ft_printf("OLDPWD not found\n");
+				// PWD
+				char 	pwd[PATH_MAX + 1];
+				// recupere le pwd
+				getcwd(pwd, PATH_MAX + 1);
+				if (!(change_env_var(&env, "PWD", pwd)))
+					ft_printf("PWD not found\n");
+				ft_printf("... moving to %s ....\n", command[1]);
+			}
+			continue;
+		}
+		else if (ft_strcmp(command[0], "env") == 0)
+		{
+			ft_lstiter(env, print_lstenv);
+			continue;
+		}
+		else if (ft_strcmp(command[0], "setenv") == 0)
+		{
+			tmplst = ft_lstnew(NULL, sizeof(t_varenv *));
+			tmplst->content = create_varenv(command[1], command[2]);
+			ft_lstaddend(&env, tmplst);
+			continue;
+		}
+		else if (ft_strcmp(command[0], "unsetenv") == 0)
+		{
+			remove_one(&env, command[1]);
+			continue;
+		}
+		else if (ft_strcmp(command[0], "echo") == 0)
+		{
+			y = 0;
+			while (command[1 + y])
+			{
+				ft_printf("%s", command[1 + y]);
+				y++;
+				if (command[1 + y])
+					ft_printf(" ");
+			}
+			ft_printf("\n");
+			continue;
+		}
+		/*
+		**	FIN DES BUILTINS
+		*/
+		// Recherche du fichier dans les repos du path
+		i = -1;
+		while (env_paths[++i])
+		{
+			// Si le fichier existe pas dans ce path, continue a boucler
+			if ((lstat(get_complete_path(env_paths[i], command[0]), &st)) < 0)
+			{
+				// Si c'etait le dernier -> pas trouve
+				if (!env_paths[i + 1])
+				{
+					ft_printf("minishell: command not found: %s\n", command[0]);
+					break;
+				}
+				continue;
+			}
+			// Si le fichier est trouvé on l'execute
+			else
+			{
+				ft_printf("tiens ton resultat de merde :\n");
+				father = fork();
+				if (father == 0)
+					execve(get_complete_path(env_paths[i], command[0]), command, environ);
+				else if (father < 0)
+					ft_printf("fail\n");
+				else if (father > 0)
+					wait(&father);
+				break;
+			}
+		}
+		//ft_printf("tiens ton resultat de merde : %s\n", line);
+		ft_strdel(&line);
+	}
 	return (0);
 }
