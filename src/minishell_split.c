07df40285efb501a52b3327ea276684b4bb58ddd
ndubouil <ndubouil@student.42.fr>
2018-11-28 19:45:22 +0100

    continue the fix

--- src/minishell_split.c
+++ src/minishell_split.c
@@ -6,7 +6,7 @@
 /*   By: ndubouil <ndubouil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 15:19:02 by ndubouil          #+#    #+#             */
-/*   Updated: 2018/11/27 19:54:07 by ndubouil         ###   ########.fr       */
+/*   Updated: 2018/11/28 17:47:31 by ndubouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,9 +43,53 @@ static int	count_commands(char *str)
 	return (j);
 }
 
-static int	get_size_of_next_command(char **str)
+static int	get_size_of_next_command(char *str)
 {
+	int i = -1;
+	int y = 0;
+	int escape = FALSE;
 
+	while (str[++i])
+	{
+		if (str[i] == '\\')
+		{
+			escape = TRUE;
+			i++;
+		}
+		if ((str[i] == ' ' || str[i] == '\t') && !escape)
+			continue;
+		else if (str[i] == '\"' && !escape)
+		{
+			y = 0;
+			while (str[i++])
+			{
+				if (str[i] == '\"')
+					break;
+				y++;
+			}
+			return (y);
+		}
+		else if (str[i] == '\'' && !escape)
+		{
+			y = 0;
+			while (str[i++])
+			{
+				if (str[i] == '\'')
+					break;
+				y++;
+			}
+			return (y);
+		}
+		y = 0;
+		while (((str[i] != ' ' && str[i] != '\t' && str[i] && str[i] != '\"' && str[i] != '\'' && str[i] != '\\') || escape) && str[i])
+		{
+			if (escape)
+				escape = FALSE;
+			y++;
+			i++;
+		}
+	}
+	return (y);
 }
 
 char		**minishell_split(char *str)
@@ -59,7 +103,7 @@ char		**minishell_split(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	if (!(result = ft_memalloc(count_commands(str) * sizeof(char *))))
+	if (!(result = ft_memalloc((count_commands(str) + 1) * sizeof(char *))))
 		return (0);
 	i = -1;
 	j = 0;
@@ -69,7 +113,8 @@ char		**minishell_split(char *str)
 	{
 		if (escape)
 			escape = FALSE;
-		if (!(result[j] = ft_strnew(10000)))
+		// ft_printf("taille: %d\n", get_size_of_next_command(&str[i]));
+		if (!(result[j] = ft_strnew((get_size_of_next_command(&str[i]) + 1) * sizeof(char))))
 			return (0);
 		if (str[i] == '\\')
 		{
