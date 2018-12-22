bc98f43580f07b30adf2e94e154e2e099f5ba005
Nico <ndubouilh@gmail.com>
2018-12-23 00:31:03 +0100

    ADD leaks fixed

--- src/ft_strtabdel.c
+++ src/ft_strtabdel.c
@@ -6,7 +6,7 @@
 /*   By: ndubouil <ndubouil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/15 17:06:27 by ndubouil          #+#    #+#             */
-/*   Updated: 2018/12/20 03:56:36 by ndubouil         ###   ########.fr       */
+/*   Updated: 2018/12/23 00:19:44 by ndubouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,14 @@ void	ft_strtabdel(char ***tab)
 {
 	int		i;
 
-	if (!*tab || !(*tab)[0])
+	if (!(*tab))
 		return ;
+	if (!(*tab)[0])
+	{
+		ft_memdel((void **)tab);
+		*tab = NULL;
+		return ;
+	}
 	i = -1;
 	while ((*tab)[++i])
 		ft_strdel(&(*tab)[i]);
